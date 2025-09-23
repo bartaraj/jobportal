df93c9344c349ca182b3989cf58e3bacfa38bec3
bartaraj <bartaraj.7037703@gpkmc.edu.np>
2025-09-23 13:16:44 +0545

    Assets included and layout created

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/homepage', function () {
+    return view('homepage');
+});
+
+Route::get('/job_listing', function () {
+    return view('job_listing');
+})->name('job_listing');
+
+Route::get('/job_details', function () {
+    return view('job_details');
+})->name('job_details');
